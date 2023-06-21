4309271e0d302518706f0e14625df1ef5c2f9747
thakur17 <upd_thakur@hotmail.com>
2023-06-21 11:58:46 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudentCrudMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentCrudMvc.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Name Please")]
+        public string Name { get; set; }
+        public int RollNo { get; set; }
+        [Required(ErrorMessage = "Class Name Please")]
+        public string ClassName { get; set; }
+    }
+}
